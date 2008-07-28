2349565bcc915dfe5a01d425f25ad9e0ea320d78
Anders Kaseorg <andersk@mit.edu>
2008-07-28 09:44:07 -0400

    Tidy nsswitch-internal.h; remove unused __nss_configure_lookup.
    Signed-off-by: Anders Kaseorg <andersk@mit.edu>

--- nsswitch-internal.h
+++ nsswitch-internal.h
@@ -6,21 +6,14 @@
 #ifndef NSSWITCH_INTERNAL_H
 #define NSSWITCH_INTERNAL_H
 
+/* glibc/nss/nsswitch.h */
 typedef struct service_user service_user;
 
-extern int
-__nss_next (service_user **ni, const char *fct_name, void **fctp, int status,
-            int all_values);
-
-extern int
-__nss_database_lookup (const char *database,
-		       const char *alternative_name,
-		       const char *defconfig, service_user **ni);
-
-extern int
-__nss_configure_lookup (const char *dbname, const char *service_line);
-
-extern void
-*__nss_lookup_function (service_user *ni, const char *fct_name);
+extern int __nss_next (service_user **ni, const char *fct_name, void **fctp,
+		       int status, int all_values);
+extern int __nss_database_lookup (const char *database,
+				  const char *alternative_name,
+				  const char *defconfig, service_user **ni);
+extern void *__nss_lookup_function (service_user *ni, const char *fct_name);
 
 #endif /* NSSWITCH_INTERNAL_H */
