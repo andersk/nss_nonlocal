d52c3f35301d10d46a842416bd30777ceb12be7f
Anders Kaseorg <andersk@mit.edu>
2018-05-05 14:04:26 -0400

    Switch from __nss_next API to __nss_next2 API
    __nss_passwd_lookup2, __nss_group_lookup2, and __nss_next2 were added
    in glibc 2.8.
    
    __nss_passwd_lookup, __nss_group_lookup, and __nss_next were removed
    in glibc 2.27.
    
    Signed-off-by: Anders Kaseorg <andersk@mit.edu>

--- nsswitch-internal.h
+++ nsswitch-internal.h
@@ -21,17 +21,20 @@
 /* glibc/nss/nsswitch.h */
 typedef struct service_user service_user;
 
-extern int __nss_next (service_user **ni, const char *fct_name, void **fctp,
-		       int status, int all_values);
+extern int __nss_next2 (service_user **ni, const char *fct_name,
+			const char *fct2_name, void **fctp, int status,
+			int all_values);
 extern int __nss_database_lookup (const char *database,
 				  const char *alternative_name,
 				  const char *defconfig, service_user **ni);
 extern void *__nss_lookup_function (service_user *ni, const char *fct_name);
 
 /* glibc/nss/XXX-lookup.c */
-extern int __nss_passwd_lookup (service_user **ni, const char *fct_name,
-				void **fctp) internal_function;
-extern int __nss_group_lookup (service_user **ni, const char *fct_name,
-			        void **fctp) internal_function;
+extern int __nss_passwd_lookup2 (service_user **ni, const char *fct_name,
+				 const char *fct2_name, void **fctp)
+    internal_function;
+extern int __nss_group_lookup2 (service_user **ni, const char *fct_name,
+				const char *fct2_name, void **fctp)
+    internal_function;
 
 #endif /* NSSWITCH_INTERNAL_H */
